99940fc61dc65746c07374d89dfe7fe666c27413
zengjunming <zengjunming@htmimi.com>
2025-03-26 23:16:16 +0800

    feat: add prettier

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 class MyWebpackPlugin {
-    apply(compiler) {
-        compiler.hooks.emit.tapAsync('MyWebpackPlugin', (compilation, callback) => {
-            console.log('MyWebpackPlugin is working!');
-            // Ensure the callback is called even if no files are emitted
-            if (typeof callback === 'function') {
-                callback();
-            }
-        });
-    }
+  apply(compiler) {
+    compiler.hooks.emit.tapAsync('MyWebpackPlugin', (compilation, callback) => {
+      console.log('MyWebpackPlugin is working!');
+      // Ensure the callback is called even if no files are emitted
+      if (typeof callback === 'function') {
+        callback();
+      }
+    });
+  }
 }
 
 export default MyWebpackPlugin;
